090a7ae4f2e3788bae37547548efc300c245d3af
bob329 <bob329@sina.com>
2023-08-17 14:13:36 +0800

    代码修正

--- 源代码/ch17/例17.3.py
+++ 源代码/ch17/例17.3.py
@@ -11,10 +11,7 @@ beta_params = [(1, 1)]
 dist = stats.beta #dist 设为 Beta 分布
 x = np.linspace(0, 1, 100)
 for idx, N in enumerate(trials):
-    if idx == 0:
-        plt.subplot(2,2, 1)
-    else:
-        plt.subplot(2, 2, idx + 1)
+    plt.subplot(2, 2, idx + 1)
     y = data[idx]
     for (a_prior, b_prior), c in zip(beta_params, ('b')):
     # 后验概率
